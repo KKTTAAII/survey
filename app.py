5649f73227dfdf18dff1d6ada27247886aef837e
Ktai <krataias65@gmail.com>
2021-09-13 10:22:43 -0600

    fifth commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, flash
+from flask.globals import session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import surveys
 
@@ -7,22 +8,23 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-user_responses = []
-current_survey = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def select_survey():
     """the user can select a survey"""
     all_surveys = [survey for survey in surveys.keys()]
     return render_template("homepage.html", surveys=all_surveys)
 
+@app.route("/session", methods = ["POST"])
+def empty_session():
+    session["responses"] = []
+    session["current_survey"] = request.form["survey"]
+    return redirect('/survey')
+
 @app.route("/survey", methods=["GET", "POST"])
 def show_survey():
     """Show the survey question and instructions"""
-    user_responses.clear()
-    current_survey.clear()
-    selected_survey = request.args["survey"]
-    current_survey.append(selected_survey)
+    selected_survey = session["current_survey"]
     title = surveys[selected_survey].title
     instructions = surveys[selected_survey].instructions
     return render_template("survey.html", title=title, inst=instructions, selected_survey=selected_survey)
@@ -32,42 +34,50 @@ def show_survey():
 def show_questions(num):
     """show the question according to the question number"""
 
+    responses_length = len(session["responses"])
+
+    question = surveys[session["current_survey"]].questions[int(num)].question
+    choices = surveys[session["current_survey"]].questions[int(num)].choices
+    text_box = surveys[session["current_survey"]].questions[int(num)].allow_text
+
+    if(responses_length == num):
+        return render_template(
+        "questions.html",
+        question=question,
+        choices=choices, text_box=text_box)
+
     """if the number exceeds the number of the questions, 
     redirect the user to the home page"""
-    if(num > len(surveys[current_survey[0]].questions)):
+    if(num > len(surveys[session["current_survey"]].questions)):
         flash(
             f"""Invalid question number. There are only 
-            {len(surveys[current_survey[0]].questions)} 
+            {len(surveys[session["current_survey"]].questions)} 
             questions for this survey"""
             , "error")
         return redirect("/")
 
     """prevent user from trying to answer the question
     in wrong order"""
-    if(len(user_responses) != num):
-        return redirect(f"/questions/{len(user_responses)}")
+    if(responses_length != num):
+        return redirect(f"/questions/{responses_length}")
 
     """check if the user has completed all the questions in the survey"""
-    if(len(user_responses) == len(surveys[current_survey[0]].questions)):
+    if(responses_length == len(surveys[session["current_survey"]].questions)):
         flash("You have completed all the questions", "finished")
         return redirect('/complete')
-
-    question = surveys[current_survey[0]].questions[int(num)].question
-    choices = surveys[current_survey[0]].questions[int(num)].choices
-    text_box = surveys[current_survey[0]].questions[int(num)].allow_text
-
-    return render_template(
-        "questions.html",
-        question=question,
-        choices=choices, text_box=text_box)
 
 
 @app.route("/answer", methods=["POST"])
 def answer():
     """store answers"""
     ans = request.form["answer"]
-    user_responses.append(ans)
-    return redirect(f"/questions/{len(user_responses)}")
+    session["responses"].append(ans)
+    length = len(session["responses"])
+    print("*********************")
+    print(session["responses"])
+    print(length)
+    print("*********************")
+    return redirect(f"/questions/{length}")
 
 
 @app.route('/complete')
